01a21507f9d5ea8dcfbbe52d8241d3ff6fc6f169
EimanTan <34124245+Eiman24@users.noreply.github.com>
2018-09-05 14:26:10 +0800

    09052018

--- VPython for Beginners 3 - Making Shapes.py
+++ VPython for Beginners 3 - Making Shapes.py
@@ -1,6 +1,7 @@
 from vpython import *
 
 sphere_location = vector(0,0,0)
-box_location = vector(2,0,0)
+box_location = vector(1,0,0)
 
-sphere(pos = sphere_location)
+sphere(pos = sphere_location, radius = 0.5, color = color.yellow)
+box(pos = box_location, size = vector(0.5,0.1,1.0), color = color.red) # size = width, height, depth
